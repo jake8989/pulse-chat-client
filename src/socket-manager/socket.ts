325f40a3397d5ddc3dae948e79bb51297950a4f4
jake <jjk848798@gmail.com>
2023-10-27 02:15:52 +0530

    fix:socket error

--- src/socket-manager/socket.ts
+++ src/socket-manager/socket.ts
@@ -1,4 +1,7 @@
 import { io, Socket } from 'socket.io-client';
-let url = process.env.NEXT_PUBLIC_BACKEND;
-const socket: Socket = io(url, { transports: ['websocket'] });
+let url = process.env.NEXT_PUBLIC_BACKEND || 'http://localhost:8000';
+console.log(url);
+let socket: Socket;
+
+socket = io(url, { transports: ['websocket'] });
 export default socket;
